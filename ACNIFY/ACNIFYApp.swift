853158e3b6f34a767d2d3e6c53fa533ca9d7bfad
Nadia Ramadhani <103446529+twicetw@users.noreply.github.com>
2022-06-22 20:07:47 +0700

    Initial Commit

---
+++ ACNIFY/ACNIFYApp.swift
@@ -0,0 +1,17 @@
+//
+//  ACNIFYApp.swift
+//  ACNIFY
+//
+//  Created by Local Administrator on 22/06/22.
+//
+
+import SwiftUI
+
+@main
+struct ACNIFYApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ACNIFYUITests/ACNIFYUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  ACNIFYUITestsLaunchTests.swift
+//  ACNIFYUITests
+//
+//  Created by Local Administrator on 22/06/22.
+//
+
+import XCTest
+
+class ACNIFYUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
