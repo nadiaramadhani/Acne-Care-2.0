7b32f059f8eafca3c97dd6c24d3e3436bb90373b
rahmatmaftuh <rahmatmaftuhi@gmail.com>
2022-06-23 11:12:49 +0700

    Tester branch

---
+++ ACNIFY/TesterBranch.swift
@@ -0,0 +1,20 @@
+//
+//  TesterBranch.swift
+//  ACNIFY
+//
+//  Created by Rahmat Maftuh Ihsan on 23/06/22.
+//
+
+import SwiftUI
+
+struct TesterBranch: View {
+    var body: some View {
+        Text("Ini branch baru")
+    }
+}
+
+struct TesterBranch_Previews: PreviewProvider {
+    static var previews: some View {
+        TesterBranch()
+    }
+}
