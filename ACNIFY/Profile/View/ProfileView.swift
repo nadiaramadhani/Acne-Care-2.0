01479c46ab21b2b46ce21cd87477a6576c04ddcf
rahmatmaftuh <rahmatmaftuhi@gmail.com>
2022-08-20 21:56:31 +0700

    TabView&SummaryPage50%
    Tab View 50% Summary page 50%, Purpose of this push for nadia apply calendar

---
+++ ACNIFY/Profile/View/ProfileView.swift
@@ -0,0 +1,20 @@
+//
+//  ProfileView.swift
+//  ACNIFY
+//
+//  Created by Rahmat Maftuh Ihsan on 20/08/22.
+//
+
+import SwiftUI
+
+struct ProfileView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ProfileView_Previews: PreviewProvider {
+    static var previews: some View {
+        ProfileView()
+    }
+}
